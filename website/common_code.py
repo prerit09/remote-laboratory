831fb235ff2e528e377503c1ef40db0dde184b1a
Prerit Jain <prerit.jain@collenda.com>
2023-07-01 18:16:18 +0200

    updated url

--- website/common_code.py
+++ website/common_code.py
@@ -5,7 +5,7 @@ from flask import Flask, redirect, url_for, render_template, request
 def result(code, input, expected=None):
     if(request.method == "POST"):
         
-        url = "http://localhost:2358/submissions?wait=true"
+        url = "http://141.24.210.95:2358/submissions?wait=true"
         
         if expected is not None:
             data = {
