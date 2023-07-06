d40bad78f0c21b7a2519f1c4c5d2c9fe2adbd8a8
Prerit Jain <prerit.jain@collenda.com>
2023-07-06 20:55:07 +0200

    required professor token

--- website/static/singUp.js
+++ website/static/singUp.js
@@ -1,6 +1,15 @@
 function professorCheck() {
     if (document.getElementById('professor').checked) {
         document.getElementById('professor_token_div').style.display = 'block';
+        setRequired(true);
     }
-    else document.getElementById('professor_token_div').style.display = 'none';
+    else {
+        document.getElementById('professor_token_div').style.display = 'none';
+        setRequired(false);        
+    }
+
+    function setRequired(val){
+        input = document.getElementById("professor_token");
+        input.required = val;
+    }
 }
